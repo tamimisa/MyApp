4f930d330fc71354ded1fb8e2ad3b1a605fc8270
tamimisa <442200424@student.ksu.edu.sa>
2023-09-06 15:17:48 +0300

    LabOfWeek3

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
-
+    Button send_button;
+    EditText send_text;
+    TextView receiver_msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        send_button= findViewById(R.id.send_button_id);
+        send_text = findViewById(R.id.send_text_id);
+        receiver_msg = (TextView) findViewById(R.id.received_value_id);
+        send_button.setOnClickListener(v -> {
+            String str = send_text.getText().toString();
+            receiver_msg.setText("Hello,"+str+"!" );
+        });
     }
 }
